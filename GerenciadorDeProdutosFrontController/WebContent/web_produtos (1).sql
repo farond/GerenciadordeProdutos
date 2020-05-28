e6abe87c9ee01438d938ffafcd700180f2def307
farond <43458363+farond@users.noreply.github.com>
2020-05-28 18:05:45 -0300

    Adicionando BDO

---
+++ GerenciadorDeProdutosFrontController/WebContent/web_produtos (1).sql
@@ -0,0 +1,12 @@
+
+CREATE DATABASE web_produtos;
+
+USE web_produtos;
+
+CREATE TABLE produto (
+	codigo INT NOT NULL PRIMARY KEY,
+    nome VARCHAR(256) NOT NULL,
+    descricao TEXT,
+    valor DOUBLE,
+    estoque INT
+);
